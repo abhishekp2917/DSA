5931216de6fcec40b024e727e18d88585d88dc85
Abhishek Pandey <abhishekp2917@gmail.com>
2022-10-18 12:20:41 +0530

    Update 2_Stack_Using_Linked_List

--- LinkedList/2_stack_using_linked_list/Solution.java
+++ LinkedList/2_stack_using_linked_list/Solution.java
@@ -1,34 +1,63 @@
-// Structure of LinkedList
-class Node
-{
-	int data;
-	Node next;
-	Node(int d)
-	{
-		data = d;
+class StackNode {
+    int data;
+    StackNode next;
+	StackNode(int a) {
+        data = a;
 		next = null;
 	}
-}
+}  
 
+class MyStack 
+{
+	// Head of the LinkedList 
+    StackNode Head;
+    
+    //Function to push an integer into the stack.
+    void push(int a) 
+    {
+		// if no node is present then just add a node to head of LinkedList
+        if(Head==null) Head = new StackNode(a);
+		// else move towards last node and add new Node there
+        else {
 
-class Solution {
-
-	public static boolean detectLoop(Node head){
-        
-		// slow and fast pointers
-		// slow pointer moves forward by one node
-		// fats pointer moves forward by two node
-        Node slow = head, fast = head.next;
-        
-		// while next's next node of fast pointer is not null 
-		// check if slow and fast pointer points to same node or not
-        while(fast!=null && fast.next!=null && fast.next.next!=null) {
-            
-			// if slow and fast pointer points to same node the loop exists else not
-            if(slow==fast) return true;
-            slow = slow.next;
-            fast = fast.next.next;
+			// create a pointer which points to head  
+            StackNode tempHead = Head;
+			// then move that pointer to next nodes until pointer next node is null
+            while(tempHead.next!=null) tempHead = tempHead.next;
+			// then create new Node with the value that is to be added to top of the stack and
+			// assign it to last node next reference 
+            tempHead.next = new StackNode(a);
         }
-        return false;
+    }
+    
+    //Function to remove an item from top of the stack.
+    int pop() 
+    {
+		// if head is null that means there is no element in stack so return -1
+        if(Head==null) return -1;
+
+		// create three pointers which points to previous , current and next node 
+		// so that when current node moves to last node then we will have referenec to second last node 
+		// and can make it null which will remove last node (top of the stack) from the stack
+        StackNode prev = null, curr = Head, next = Head.next;
+        
+		// move all pointers forward until reference to next node is null so that current will move
+		// to last node (top) of the stack  
+        while(next!=null) {
+            prev = curr;
+            curr = next;
+            next = next.next;
+        }
+        
+		
+		// if previous node is not null then make previous next reference to null so that it will
+		// remove last (top) node from the stack  
+        if(prev!=null) prev.next = null;
+		// if previous node is null that means current is pointing to head and there is only one
+		// node in stack so just remove the current (head) node
+        else Head = null;
+        
+		// return current node as it will point to last (top) node of the stack
+        return curr.data;
     }
 }
